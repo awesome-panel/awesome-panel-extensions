06e638b070246da94e2699bdd7e35868ce8805ce
Marc Skov Madsen <MASMA@orsted.dk>
2020-08-02 06:31:58 +0200

    added awesome panel designer - all tests pass

---
+++ awesome_panel_extensions/developer_tools/designer/__init__.py
@@ -0,0 +1,3 @@
+"""Exports"""
+from awesome_panel_extensions.developer_tools.designer.designer import Designer
+from awesome_panel_extensions.developer_tools.designer.services.reload_service import ReloadService
---
+++ awesome_panel_extensions/developer_tools/designer/services/__init__.py
@@ -0,0 +1,2 @@
+"""Exports"""
+from awesome_panel_extensions.developer_tools.designer.services.reload_service import ReloadService
