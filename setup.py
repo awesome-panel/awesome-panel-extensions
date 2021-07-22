25d051ac8ddb5b31b0820cd872d92d7057ff6626
Marc Skov Madsen <marc.skov.madsen@gmail.com>
2021-07-22 11:28:20 +0200

    upgrade bokeh + release new version

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ extras_require["all"] = sorted(set(sum(extras_require.values(), [])))
 
 setuptools.setup(
     name="awesome-panel-extensions",
-    version="20210515.1",
+    version="20210722.2",
     description="A package of awesome Panel extensions. Provided by awesome-panel.org",
     long_description=long_description,
     long_description_content_type="text/markdown",
